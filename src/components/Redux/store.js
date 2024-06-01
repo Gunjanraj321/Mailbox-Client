8a0e0d2bc7924296463fb22d3fdbb07e3e9a9e61
gunjanraj321 <gunjanraj321@gmail.com>
2024-06-01 18:51:38 +0530

    store updated

--- src/components/Redux/store.js
+++ src/components/Redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./AuthSlice";
 import MailReducer from './mailSlice';
-// import ErrorReducer from './errorSlice'
 
 const store = configureStore({
     reducer:{
         auth:AuthReducer,
         mail:MailReducer,
-        // error:ErrorReducer,
     }
 })
 
